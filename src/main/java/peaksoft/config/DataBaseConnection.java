96a1719b8866bd13a96449b7fa54c5da1b75af21
eldiuar <avazbekuulualdiar326@gmail.com>
2024-05-04 12:25:49 +0300

    try catch koshtum

--- src/main/java/peaksoft/config/DataBaseConnection.java
+++ src/main/java/peaksoft/config/DataBaseConnection.java
@@ -14,7 +14,7 @@ import java.util.Properties;
             properties.put(Environment.URL, "jdbc:postgresql://localhost:5432/postgres");
             properties.put(Environment.USER, "postgres");
             properties.put(Environment.PASS, "1234");
-            properties.put(Environment.HBM2DDL_AUTO, "create");
+            properties.put(Environment.HBM2DDL_AUTO, "update");
             properties.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQLDialect");
             properties.put(Environment.SHOW_SQL, "true");
             Configuration configuration = new Configuration();
